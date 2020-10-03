a04233c36200e75ca4f112620d24e1e130797951
medha <medha@Medha-PC>
2020-10-03 13:30:17 -0400

    Added payment and shipping models and JSP form and updated Purchase

--- iMarket/src/edu/osu/cse5234/controller/Purchase.java
+++ iMarket/src/edu/osu/cse5234/controller/Purchase.java
@@ -9,7 +9,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/purchase")
 public class Purchase {
+	
+	@RequestMapping(path = "/paymentEntry", method = RequestMethod.GET)
+	public String viewPaymentEntryForm(HttpServletRequest request, HttpServletResponse response) {
+		request.setAttribute("paymentInfo", new PaymentInfo());	
+		return "PaymentEntryForm";
+	}
+	
+	@RequestMapping(path = "/submitPayment", method = RequestMethod.POST)
+	public String submitPayment(@ModelAttribute("paymentInfo") PaymentInfo paymentInfo, HttpServletRequest request) {
+		request.getSession().setAttribute("paymentInfo", paymentInfo);
+		return "redirect:/purchase/shippingEntry";
+	}
+
+	@RequestMapping(path = "/shippingEntry", method = RequestMethod.GET)
+	public String viewShippingEntryForm(HttpServletRequest request, HttpServletResponse response) {
+		request.setAttribute("shippingInfo", new ShippingInfo());	
+		return "ShippingEntryForm";
+	}
+
+	@RequestMapping(path = "/submitShipping", method = RequestMethod.POST)
+	public String submitShipping(@ModelAttribute("shippingInfo") ShippingInfo shippingInfo, HttpServletRequest request) {
+		request.getSession().setAttribute("shippingInfo", shippingInfo);
+		return "redirect:/purchase/viewOrder";
+	}	
+	
 	@RequestMapping(path="/viewOrder", method = RequestMethod.GET)
 	String getOrderDetails() {
 		return "ViewOrder";
