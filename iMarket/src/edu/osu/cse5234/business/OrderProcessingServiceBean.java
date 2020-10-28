3b31333501cb1f8447bd689f3e0baf6957918a4a
hp <hp@LAPTOP-EIS1BVEL.lan1>
2020-10-28 18:32:13 -0400

    Objective 6

--- iMarket/src/edu/osu/cse5234/business/OrderProcessingServiceBean.java
+++ iMarket/src/edu/osu/cse5234/business/OrderProcessingServiceBean.java
@@ -3,6 +3,8 @@ package edu.osu.cse5234.business;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 
+import edu.osu.cse5234.business.view.Item;
+import edu.osu.cse5234.models.LineItem;
 import edu.osu.cse5234.models.Order;
 import edu.osu.cse5234.util.ServiceLocator;
 
@@ -16,25 +18,54 @@ public class OrderProcessingServiceBean {
     /**
      * Default constructor. 
      */
+	@PersistenceContext
+	EntityManager entityManager;
+	
+	public EntityManager getEntityManager() {
+		return entityManager;
+	}
+
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+	
     public OrderProcessingServiceBean() {
         // TODO Auto-generated constructor stub
     }
     public String processOrder(Order order) {
     	
-    	if(ServiceLocator.getInventoryService().validateQuantity(order.getItems()))
-    	{
-    		ServiceLocator.getInventoryService().updateInventory(order.getItems());
-    		
+//    	if(ServiceLocator.getInventoryService().validateQuantity(order.getItems()))
+//    	{
+//    		ServiceLocator.getInventoryService().updateInventory(order.getItems());
+//    		
+//    	}
+//    	int range=9999-1111;
+//    	int conf=(int)(1110+Math.random()*(range));
+//    	System.out.println(conf+"  ***************\n");
+//    	return ""+conf;
+    	
+    	if (validateItemAvailability(order)) {
+    		entityManager.persist(order);
+    		entityManager.flush();
     	}
-    	int range=9999-1111;
-    	int conf=(int)(1110+Math.random()*(range));
-    	System.out.println(conf+"  ***************\n");
-    	return ""+conf;
     }
     
     public boolean validateItemAvailability(Order order) {
     		
-    	return ServiceLocator.getInventoryService().validateQuantity(order.getItems());
+    	// return ServiceLocator.getInventoryService().validateQuantity(order.getItems());
+    	List<Item> items = ServiceLocator.getInventoryService().getAvailableInventory().getItems();
+    	for(int i = 0; i< order.getLineItems()i; i++) {
+    		LineItem x = order.getLineItems().get(i);
+    		for(Item y: items) {
+    			if(x.getItem_number() == y.getItemNumber()) {
+    				if(x.getQuantity() > y.getAvailable_quantity())
+    					return false;
+    				else
+    					break;
+    			}    				
+    		}
+    	}
+    	return true;
     }
     
 
